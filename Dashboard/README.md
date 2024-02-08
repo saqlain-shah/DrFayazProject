c41addd5617811a562c907700fec7a7ccf317233
Saqlain Shah <saqlainshahbaltee@gmail.com>
2024-02-08 10:52:32 +0500

    upadated the folder structure

---
+++ Dashboard/README.md
@@ -0,0 +1,48 @@
+# Project Name
+
+Dr Fayaz Project
+## Author
+
+- [Github](https://github.com/saqlain-shah)
+- Contact: [Email](mailto:saqlainshahbaltee@gmail.com.com)
+
+## Description
+
+The Dental Admin Dashboard Template is a comprehensive and feature-rich solution designed specifically for clinic. It provides a powerful and intuitive interface for managing various aspects of clinic operations, streamlining administrative tasks, and enhancing overall efficiency.
+
+## Installation
+
+1. Open folder in code editor
+
+2. Install project dependencies:
+
+```shell
+npm install
+```
+
+## Usage
+
+1. Start the development server:
+
+```shell
+npm start
+```
+
+2. Open your browser and visit `http://localhost:3000` to view the application.
+
+## Deployment
+
+- Contact: [Email](mailto:saqlainshahbaltee@gmail.com.com)
+
+## Technologies Used
+
+- React
+- Tailwind Css
+
+## Contributing
+
+- Contact: [Email](mailto:saqlainshahbaltee@gmail.com.com)
+
+## License
+
+- Contact: [Email](mailto:saqlainshahbaltee@gmail.com.com)
---
+++ Website/src/components/Contact/Contact.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import Footer from '../Shared/Footer/Footer'
+import { useForm } from 'react-hook-form';
+import { FaLocationArrow, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
+import Header from '../Shared/Header/Header';
+import './index.css';
+import SubHeader from '../Shared/SubHeader';
+
+const Contact = () => {
+    const { register, handleSubmit } = useForm({});
+    const onSubmit = (data) => {
+
+    };
+    return (
+        <>
+            <Header />
+            <SubHeader title="Contact us" subtitle="Lorem ipsum dolor sit amet consectetur adipisicing." />
+            <section id="contact" className="contact mt-5 mb-5">
+                <div>
+                    <div className="container">
+                    {/* eslint-disable-next-line */}
+                        <iframe style={{ border: 0, width: "100%", height: "350px" }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div className="container" style={{ marginTop: 80, marginBottom: 120 }}>
+                    <div className="row">
+
+                        <div className="col-lg-4">
+                            <div className="info rounded p-3" style={{ background: '#f8f9fa' }}>
+                                <div className="d-flex mb-2 gap-2">
+                                    <FaLocationArrow className='icon' />
+                                    <div>
+                                        <h4>Location:</h4>
+                                        <p>1212 Dhaka, Kazi Nozrul Avenur, Sylhet, Bangladesh 03214</p>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex mb-2 gap-2">
+                                    <FaEnvelope className='icon' />
+                                    <div>
+                                        <h4>Email:</h4>
+                                        <p>ujjalzaman@gmail.com</p>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex mb-2 gap-2">
+                                    <FaPhoneAlt className='icon' />
+                                    <div>
+                                        <h4>Call:</h4>
+                                        <p>+88 01751 040425</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="col-lg-8">
+                            <div className="mb-5 p-2 rounded" style={{ background: '#f8f9fa' }}>
+                                <form className="row form-row" onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="col-md-6">
+                                        <div className="form-group mb-2 card-label">
+                                            <label>First Name</label>
+                                            <input type="text" name="" id="" placeholder="First Name" className="form-control mb-3" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-6">
+                                        <div className="form-group mb-2 card-label">
+                                            <label>Last Name</label>
+                                            <input type="text" name="" id="" placeholder="Last Name" className="form-control mb-3" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-12">
+                                        <div className="form-group mb-2 card-label">
+                                            <label>Email</label>
+                                            <input type="email" name="" id="" placeholder="Email" className="form-control mb-3" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-12">
+                                        <div className="form-group mb-2 card-label">
+                                            <label>Subject</label>
+                                            <input type="text" name="" id="" placeholder="enter your subject" className="form-control mb-3" />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-12">
+                                        <div className="form-group">
+                                            <label className='form-label'>Subject</label>
+                                            <textarea name="" id="" cols="30" rows="10" placeholder="enter your message" className="form-control mb-3" />
+                                        </div>
+                                    </div>
+
+                                    <div className="text-center mt-3 mb-5">
+                                        <button type='submit' className="appointment-btn">Send Message</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    )
+}
+
+export default Contact
