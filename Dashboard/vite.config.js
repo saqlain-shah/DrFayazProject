b21780ab57fddc122e3b75ec126ac7e05bbbc982
sAjU1122 <davbabu1122@gmail.com>
2024-02-08 22:13:59 +0500

    Fix Dashboard style

--- Dashboard/vite.config.js
+++ Dashboard/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-    plugins:[react()],
-    esbuild: {
-        loader: 'jsx' // Change this line
-    }
+  plugins: [
+    react(),
+    tailwindcss('./tailwind.config.js'),
+  ],
+  esbuild: {
+    loader: 'jsx', // Change this line
+  },
 });
