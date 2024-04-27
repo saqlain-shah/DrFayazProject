39eb7b68db735800db93e855b25ce3f940a9b1c3
Saqlain Shah <saqlainshahbaltee@gmail.com>
2024-04-27 16:05:56 +0500

    merge

--- Dashboard/src/screens/Register.jsx
+++ Dashboard/src/screens/Register.jsx
@@ -4,8 +4,7 @@ import { BiLogInCircle } from 'react-icons/bi';
 import { useNavigate, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
-import pic from '../build/images/logo.jpg';
-
+import pic from '../build/images/logo.jpg'
 function Register() {
     const navigate = useNavigate();
     const [name, setName] = useState('');
@@ -13,7 +12,6 @@ function Register() {
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [errors, setErrors] = useState({});
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -22,37 +20,8 @@ function Register() {
         }
     }, []);
 
-    const validateForm = () => {
-        const errors = {};
-
-        if (!name.trim()) {
-            errors.name = 'Name is required';
-        } else if (name.length < 4) {
-            errors.name = 'Name must be at least 4 characters long';
-        }
-
-        if (!email.trim()) {
-            errors.email = 'Email is required';
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            errors.email = 'Email is invalid';
-        }
-
-        if (!password.trim()) {
-            errors.password = 'Password is required';
-        } else if (password.length < 8) {
-            errors.password = 'Password must be at least 8 characters long';
-        }
-
-        setErrors(errors);
-
-        return Object.keys(errors).length === 0;
-    };
-
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!validateForm()) {
-            return;
-        }
         setLoading(true);
         try {
             const response = await axios.post('https://server-yvzt.onrender.com/api/auth/register', {
@@ -107,7 +76,6 @@ function Register() {
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                     />
-                    {errors.name && <span className="text-red-500">{errors.name}</span>}
                     <Input
                         label="Email"
                         type="email"
@@ -116,7 +84,6 @@ function Register() {
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
-                    {errors.email && <span className="text-red-500">{errors.email}</span>}
                     <Input
                         label="Password"
                         type="password"
@@ -125,7 +92,6 @@ function Register() {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    {errors.password && <span className="text-red-500">{errors.password}</span>}
                 </div>
                 <div className="flex justify-between items-center">
                     <Button
@@ -134,68 +100,17 @@ function Register() {
                         disabled={loading}
                         type="submit"
                     />
-                    
+                 
                 </div>
                 <div className="text-center mt-4">
-                    <span>Already have an account? </span>
+                    <span>Create an account? </span>
                     <button className="text-blue-500" onClick={handleLoginClick}>
-                        Login
+                    Login
                     </button>
                 </div>
             </form>
         </div>
-        
     );
 }
 
 export default Register;
-
-function Modal({ closeModal, isOpen, width, children, title }) {
-    return (
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-  
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-300"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel
-                  className={`w-full ${width ? width : 'max-w-4xl'
-                    } transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all`}
-                >
-                  <div className="w-full flex-btn gap-2 mb-8">
-                    <h1 className="text-md font-semibold">{title}</h1>
-                    <button
-                      onClick={closeModal}
-                      className="w-14 h-12 bg-dry text-red-600 rounded-md flex-colo"
-                    >
-                      <FaTimes />
-                    </button>
-                  </div>
-                  {children}
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    );
-  }
