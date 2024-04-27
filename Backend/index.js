4a3f00ea99701d5c9033a00ccde67dee42bf1860
Saqlain Shah <saqlainshahbaltee@gmail.com>
2024-04-27 12:54:34 +0500

    fix cors issue

--- Backend/index.js
+++ Backend/index.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 //     credentials: true // If you're including credentials in your requests
 // }));
 const corsOptions = {
-    origin: ['https://dashboard.avicenahealthcare.com/login', 'https://www.avicenahealthcare.com'],
+    origin: ['https://dashboard.avicenahealthcare.com', 'https://www.avicenahealthcare.com'],
     credentials: true, // You may need to include this if your requests include credentials (e.g., cookies)
 };
 
