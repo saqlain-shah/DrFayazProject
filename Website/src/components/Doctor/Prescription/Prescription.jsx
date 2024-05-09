91c2970532fead523feff4d77546a05a4f52d88c
sAjU1122 <davbabu1122@gmail.com>
2024-05-09 08:54:05 +0500

    adjust patient detail

--- Website/src/components/Doctor/Prescription/Prescription.jsx
+++ Website/src/components/Doctor/Prescription/Prescription.jsx
@@ -1,95 +1,100 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Tag, message } from 'antd';
-import { FaRegEye, FaEdit, FaRegTimesCircle } from "react-icons/fa";
-import dayjs from 'dayjs';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import DashboardLayout from '../DashboardLayout/DashboardLayout';
-import CustomTable from '../../UI/component/CustomTable';
-import { useDeletePrescriptionMutation } from '../../../redux/api/prescriptionApi';
+import React, { useEffect, useState } from "react";
+import { message } from "antd";
+import dayjs from "dayjs";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import DashboardLayout from "../DashboardLayout/DashboardLayout";
+import CustomTable from "../../UI/component/CustomTable";
+import { useDeletePrescriptionMutation } from "../../../redux/api/prescriptionApi";
 
 const Prescription = () => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [deleteBlog] = useDeletePrescriptionMutation();
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [deleteBlog] = useDeletePrescriptionMutation();
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    console.log("ID:", id);
+    fetchData();
+  }, [id]); // Refetch data when ID changes
 
-    const fetchData = async () => {
-        try {
-            setLoading(true);
-            const token = localStorage.getItem("token");
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            };
-            const response = await axios.get("http://localhost:8800/api/web/", config);
-            setData(response.data);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            // Handle error, e.g., show an error message to the user
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const deleteHandler = async (id) => {
-        message.loading("Deleting ...");
-        try {
-            const res = await deleteBlog(id);
-            if (res) {
-                message.success("Successfully Deleted !!");
-                fetchData(); // Fetch data again after deletion
-            }
-        } catch (error) {
-            message.error(error.message);
-        }
-    };
-
-    const columns = [
-        {
-            title: 'Patient Name',
-            dataIndex: 'patientInfo',
-            key: 'patientName',
-            render: patientInfo => patientInfo.name
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
-        {
-            title: 'Service',
-            dataIndex: 'selectedService',
-            key: 'selectedService',
-            render: selectedService => selectedService.serviceName
-        },
-        {
-            title: 'Service Price',
-            dataIndex: 'selectedService',
-            key: 'selectedService',
-            render: selectedService => selectedService.price
-        },
-        {
-            title: 'Date',
-            dataIndex: 'createdAt',
-            key: 'createdAt',
-            render: createdAt => dayjs(createdAt).format('YYYY-MM-DD HH:mm:ss')
-        }
-    ];
+      };
+      const response = await axios.get(
+        `https://server-yvzt.onrender.com/api/web/web/${id}`,
+        config
+      );
+      console.log("response", response);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Handle error, e.g., show an error message to the user
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <DashboardLayout>
-            <div className="w-100 mb-3 rounded" style={{ background: '#f8f9fa' }}>
-                <CustomTable
-                    columns={columns}
-                    dataSource={data}
-                    loading={loading}
-                    showPagination={true}
-                    pageSize={20}
-                    showSizeChanger={true}
-                />
-            </div>
-        </DashboardLayout>
-    );
+  const deleteHandler = async (id) => {
+    message.loading("Deleting ...");
+    try {
+      const res = await deleteBlog(id);
+      if (res) {
+        message.success("Successfully Deleted !!");
+        fetchData(); // Fetch data again after deletion
+      }
+    } catch (error) {
+      message.error(error.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Patient Name",
+      dataIndex: "patientInfo",
+      key: "patientName",
+      render: (patientInfo) => patientInfo.name,
+    },
+    {
+      title: "Service",
+      dataIndex: "selectedService",
+      key: "selectedService",
+      render: (selectedService) => selectedService.serviceName,
+    },
+    {
+      title: "Service Price",
+      dataIndex: "selectedService",
+      key: "selectedService",
+      render: (selectedService) => selectedService.price,
+    },
+    {
+      title: "Date",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (createdAt) => dayjs(createdAt).format("YYYY-MM-DD HH:mm:ss"),
+    },
+  ];
+
+  return (
+    <DashboardLayout>
+      <div className="w-100 mb-3 rounded" style={{ background: "#f8f9fa" }}>
+        <CustomTable
+          columns={columns}
+          dataSource={data}
+          loading={loading}
+          showPagination={true}
+          pageSize={20}
+          showSizeChanger={true}
+        />
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Prescription;
