87a64927144b5674764ab47637a4da62c5a37c3f
sAjU1122 <davbabu1122@gmail.com>
2024-08-12 10:33:31 +0500

    fix slot

--- Backend/index.js
+++ Backend/index.js
@@ -104,7 +104,7 @@ agenda.define('manage slots', async job => {
 agenda.on('ready', async () => {
     console.log('Agenda is ready. Scheduling jobs...');
     try {
-        await agenda.every('50 9 * * *', 'manage slots');
+        await agenda.every('50 10 * * *', 'manage slots');
         await agenda.start();
         console.log('Agenda started and job scheduled.');
     } catch (error) {
