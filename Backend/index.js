0b41dacf9b4daaa2b38220f279562b7334b67f47
Sajjad Hussain <davbabu1122@gmail.com>
2024-11-04 13:07:10 +0500

    try deploy

--- Backend/index.js
+++ Backend/index.js
@@ -101,7 +101,7 @@ agenda.define('manage slots', async job => {
     const startMinute = 0; 
     const endHour = 22;   // End hour in GMT (10:00 PM GMT)
     const endMinute = 0;  
-    const slotDuration = 30; // Slot duration in minutes
+    const slotDuration = 20; // Slot duration in minutes
 
     const slots = getSlotsForSpecificPeriod(startHour, startMinute, endHour, endMinute, slotDuration);
 
