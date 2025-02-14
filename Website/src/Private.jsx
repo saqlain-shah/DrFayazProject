5a8680c75d7ff671db5d90fd87789dd19a23ca3b
Farrukh Hussain Balay <FarrukhHGithub>
2025-02-14 14:55:09 +0500

    more changes done 8th time

--- Website/src/Private.jsx
+++ Website/src/Private.jsx
@@ -3,10 +3,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext'; // Import useAuth hook
 
 const PrivateRoute = ({ element, ...props }) => {
-    const { user } = useAuth(); // Get the user from the authentication context
-
-    // If the user is authenticated, render the provided element
-    // Otherwise, redirect to the login page
+    const { user } = useAuth();
     return (
         <Routes>
             <Route {...props}>
